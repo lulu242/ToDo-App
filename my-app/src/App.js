71d2081f1d48538ad3e59d86d7d5775a05dd3817
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-04-12 17:30:11 +0900

    feat/초기데이터 불러오기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getData } from './Actions';
 
 function App() {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data.map((el) => {
+        return <div key={el.id}>{el.title}</div>;
+      })}
+    </>
   );
 }
 
